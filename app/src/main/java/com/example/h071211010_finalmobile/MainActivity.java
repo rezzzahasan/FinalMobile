a51f1c34a575c45933b1ba0b27d655b8bf313fb3
reza <muhammadrezahasan1173@gmail.com>
2023-06-09 13:20:12 +0800

    Update

--- app/src/main/java/com/example/h071211010_finalmobile/MainActivity.java
+++ app/src/main/java/com/example/h071211010_finalmobile/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.h071211010_finalmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+ImageView btnMovie, btnFavorite, btnTvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnMovie = findViewById(R.id.btnMovie);
+        btnFavorite =  findViewById(R.id.btnFavorite);
+        btnTvShow = findViewById(R.id.btnTvShow);
+
+
     }
 }
